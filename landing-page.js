425e81121890340de4607d84c654862d6e2c6b2d
Babatunde <d2ndeayoade@gmail.com>
2024-06-30 18:47:05 +0100

    added a javascript file and wrote the code to valid the user input for newsletter form and storing the user input into an array for later use.

---
+++ landing-page.js
@@ -0,0 +1,30 @@
+// alert('I dey work o')
+
+let subscribeForm = document.getElementById('subscribeForm');
+let subscribeInput = document.getElementById('subscribeInput');
+let subscribeError = document.getElementById('subscribeError');
+
+
+let subscribers = [];
+
+subscribeForm.addEventListener('submit', (event)=>{
+  event.preventDefault();
+
+  let formIsValid = true;
+
+  if (subscribeInput.value.trim() === '') {
+    subscribeError.textContent = 'Please enter your email address';
+
+    formIsValid = false
+
+    setTimeout(() => {
+      subscribeError.textContent = '';
+    }, 2000);
+   
+  }else{
+    subscribers.push(subscribeInput.value)
+    subscribeInput.value = ''
+  }
+
+  console.log(subscribers);
+})
