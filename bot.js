57a4a33547607c93b93902d5598cbb9497444874
Babatunde <d2ndeayoade@gmail.com>
2024-07-04 03:12:46 +0100

    i add a javascript file to the board of trusty page and add event listener to display name on the view details button.

---
+++ bot.js
@@ -0,0 +1,70 @@
+// alert('i dey work o')
+
+
+let joshua = document.getElementById('joshua')
+let dami = document.getElementById('dami')
+let shalom = document.getElementById('shalom')
+let okunola = document.getElementById('okunola')
+let obot = document.getElementById('obot')
+let ubong = document.getElementById('ubong')
+let jude = document.getElementById('jude')
+
+
+
+
+
+joshua.addEventListener('click', () => {
+  joshua.textContent = 'Joshua oluwaseun Aruleba'
+
+  setTimeout(()=>{
+    joshua.textContent = 'VIEW DETAILS '
+  },3000)
+})
+
+dami.addEventListener('click', () => {
+  dami.textContent = 'Babatunde Noah Ayoade'
+
+  setTimeout(()=>{
+    dami.textContent = 'VIEW DETAILS '
+  },3000)
+})
+
+shalom.addEventListener('click', () => {
+  shalom.textContent = 'Shalom Abiodun Michael'
+
+  setTimeout(()=>{
+    shalom.textContent = 'VIEW DETAILS '
+  },3000)
+})
+
+okunola.addEventListener('click', () => {
+  okunola.textContent = 'Michael Okunola'
+
+  setTimeout(()=>{
+    okunola.textContent = 'VIEW DETAILS '
+  },3000)
+})
+
+obot.addEventListener('click', () => {
+  obot.textContent = 'Obot Eyo Michael'
+
+  setTimeout(()=>{
+    obot.textContent = 'VIEW DETAILS '
+  },3000)
+})
+
+ubong.addEventListener('click', () => {
+  ubong.textContent = 'Ubong Livinus Nyong'
+
+  setTimeout(()=>{
+    ubong.textContent = ' VIEW DETAILS'
+  },3000)
+})
+
+jude.addEventListener('click', () => {
+  jude.textContent = 'Jude iseniyi'
+
+  setTimeout(()=>{
+    jude.textContent = 'VIEW DETAILS '
+  },3000)
+})
